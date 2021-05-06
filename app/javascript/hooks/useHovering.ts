f8a078de5c305a61b4317277ce78c7401699928d
fusagiko / takayamaki <takayamaki@users.noreply.github.com>
2021-05-06 22:54:47 +0900

    yarn test:lint:js --fix

--- app/javascript/hooks/useHovering.ts
+++ app/javascript/hooks/useHovering.ts
@@ -3,8 +3,8 @@ import { useCallback, useState } from 'react';
 export const useHovering = () => {
   const [hovering, setHovering] = useState<boolean>(false);
 
-  const handleMouseEnter = useCallback(() => setHovering(true), [setHovering])
-  const handleMouseLeave = useCallback(() => setHovering(false), [setHovering])
+  const handleMouseEnter = useCallback(() => setHovering(true), [setHovering]);
+  const handleMouseLeave = useCallback(() => setHovering(false), [setHovering]);
 
   return { hovering, handleMouseEnter, handleMouseLeave };
 };
